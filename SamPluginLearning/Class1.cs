5127c56932eee3c17897e5dbab1f6881a1b2805d
Sam Rothwell <sam.rothwell@gmail.com>
2022-05-22 11:13:19 +0100

    Add project files.

---
+++ SamPluginLearning/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamPluginLearning
+{
+    public class Class1
+    {
+    }
+}
